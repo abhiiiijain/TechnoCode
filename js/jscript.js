4fd633a940316e34b7bca3ef53a8efb02d84ee45
Abhinandan Jain <abhinandanbansal123@gmail.com>
2023-08-04 20:58:54 +0530

    project uploded

---
+++ js/jscript.js
@@ -0,0 +1,14 @@
+function yescheck()
+{
+	if(document.getElementById('check').checked){
+		document.getElementById('tarea').style.display="inline-block";
+	}
+	else
+	{
+		document.getElementById('tarea').style.display="none";
+		document.getElementById('tarea').removeAttribute="required";
+	}
+}
+document.getElementById('frm').setAttribute("method","post");
+document.getElementById('n').removeAttribute('class');
+
