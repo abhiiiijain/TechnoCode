22ab23996ba934b52d4d3d4edf721431e064c3c0
Abhinandan Jain <abhinandanbansal123@gmail.com>
2024-07-27 13:20:19 +0530

    formatting done

--- js/jscript.js
+++ js/jscript.js
@@ -1,14 +1,10 @@
-function yescheck()
-{
-	if(document.getElementById('check').checked){
-		document.getElementById('tarea').style.display="inline-block";
-	}
-	else
-	{
-		document.getElementById('tarea').style.display="none";
-		document.getElementById('tarea').removeAttribute="required";
-	}
+function yescheck() {
+  if (document.getElementById("check").checked) {
+    document.getElementById("tarea").style.display = "inline-block";
+  } else {
+    document.getElementById("tarea").style.display = "none";
+    document.getElementById("tarea").removeAttribute = "required";
+  }
 }
-document.getElementById('frm').setAttribute("method","post");
-document.getElementById('n').removeAttribute('class');
-
+document.getElementById("frm").setAttribute("method", "post");
+document.getElementById("n").removeAttribute("class");
